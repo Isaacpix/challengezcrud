93187a467e99946d63db2c661bf1bb15fc3958c1
Isaacpix <80851955+Isaacpix@users.noreply.github.com>
2022-05-06 02:08:24 -0300

    2

--- crud/challenge.js
+++ crud/challenge.js
@@ -24,3 +24,50 @@ const createClient = (client) => {
     setLocalStorage (dbClient)
 }
 
+// Interação com o layout 
+
+const clearFields = () => {
+    const fields = document.querySelectorAll ('.model-field')
+    fields.forEach (field => field.value = "" )
+}
+
+const saveClient = () => {
+    if (isValidFields()) {
+        const client = {
+            nome: document.getElementById ('nome').value,
+            email: document.getElementById ('email').value,
+            celular: document.getElementById ('celular').value,
+            cidade: document.getElementById ('cidade').value
+
+        
+        }
+        createClient(client)
+        closeModal()
+
+    }
+}
+
+const createRow = (client) => {
+    const newRow = document.createElement ('tr')
+    newRow.innerHTML = 
+         // sem HTML :)  ^
+}
+
+const updateTable = () => {
+    const dbClient = readClient()
+    dbClient.forEach(createRow)
+}
+
+updateTable()
+
+
+// Eventos
+
+document.getElementById ('cadastrarCliente')
+    .addEventListener ('click', openModal)
+
+document.getElementById ('modalClose')
+    .addEventListener ('click', closeModal)
+
+document.getElementById ('salvar')
+    .addEventListener ('click', salveClient)
